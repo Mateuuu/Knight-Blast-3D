f33baf63a7947f51fcb69aee8e4c08f16f436336
Mateuuu <80788450+Mateuuu@users.noreply.github.com>
2021-12-02 22:03:03 -0700

    Progress on object pooling

--- Knight Blast 3D/Assets/Scripts/ObjectPool.cs
+++ Knight Blast 3D/Assets/Scripts/ObjectPool.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public static class ObjectPool
 {
-    // Start is called before the first frame update
-    void Start()
+    static private Queue<GameObject> objectsInPool = new Queue<GameObject>();
+    static private Dictionary<KeyValuePair<string, GameObject>, Queue<GameObject>> poolDictionary = new Dictionary<KeyValuePair<string, GameObject>,Queue<GameObject>>();
+    public static void NewPool(string name, GameObject ObjectToPool)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
     {
-        
+        if(objectsInPool.Count == 0)
+        {
+            poolDictionary[[tag]]
+            AddObjects(tag, 1, position, rotation);
+        }
+
+    }
+    static void AddObjects(string tag, int count, Vector3 position, Quaternion rotation)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            Object.Instantiate(poolDictionary[].Dequeue());
+        }
+    }
+
+    public static void ReturnToPool()
+    {
+
     }
 }
