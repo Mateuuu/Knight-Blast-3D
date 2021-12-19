ad484abc6159dba3aa06c4e49c27a8a2a8eb80e4
Mateuuu <80788450+Mateuuu@users.noreply.github.com>
2021-12-18 21:56:32 -0700

    More Inupt Stuff and Brainstorming

--- Knight Blast 3D/Assets/Scripts/PlayerArrowBehavior.cs
+++ Knight Blast 3D/Assets/Scripts/PlayerArrowBehavior.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class PlayerArrowBehavior : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
+    Rigidbody rb;
+    [SerializeField] private float arrowLifetime = 2f;
+    private float arrowTimer;
+    [SerializeField] private float arrowSpeed = 5f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
     private void OnEnable()
     {
-        rb.AddForce(transform.forward ,ForceMode.Impulse);
+        rb.AddForce(-transform.right * arrowSpeed ,ForceMode.Impulse);
     }
     private void OnDisable()
     {
         rb.velocity = new Vector3(0, 0, 0);
+    }
+    private void Update()
+    {
+        arrowTimer += Time.deltaTime;
+        if(arrowTimer >= arrowLifetime)
+        {
+            ObjectPool.ReturnToPool("PlayerArrow", this.gameObject);
+            arrowTimer = 0f;
+        }
     }
 }
--- Knight Blast 3D/Assets/Scripts/PlayerShooting.cs
+++ Knight Blast 3D/Assets/Scripts/PlayerShooting.cs
@@ -11,19 +11,19 @@ public class PlayerShooting : MonoBehaviour
 
         if(Input.GetButtonDown("Right"))
         {
-            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(1, 5, 1), Quaternion.Euler(0, 0, 0));
+            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(0, 0, 0), Quaternion.Euler(0, 180, 0));
         }
         else if(Input.GetButtonDown("Left"))
         {
-            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(1, 5, 1), Quaternion.Euler(0, 180, 0));
+            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
         }
         else if(Input.GetButtonDown("Up"))
         {
-            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(1, 5, 1), Quaternion.Euler(0, 90, 0));
+            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(0, 0, 0), Quaternion.Euler(0, 90, 0));
         }
         else if(Input.GetButtonDown("Down"))
         {
-            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(1, 5, 1), Quaternion.Euler(0, 270, 0));
+            ObjectPool.SpawnFromPool("PlayerArrow", new Vector3(0, 0, 0), Quaternion.Euler(0, 270, 0));
         }
     }
 }
