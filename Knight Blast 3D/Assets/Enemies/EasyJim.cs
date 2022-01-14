4ea1a49ca9a58b747ae3ad3b880de2b324e4a9e9
Mateuuu <80788450+Mateuuu@users.noreply.github.com>
2022-01-13 20:40:29 -0700

    ANGLES

--- Knight Blast 3D/Assets/Enemies/EasyJim.cs
+++ Knight Blast 3D/Assets/Enemies/EasyJim.cs
@@ -7,13 +7,14 @@ public class EasyJim : BaseEnemyBehavior
     // Basically a start method due to object pooling.
     private void OnEnable()
     {
-        
+        TargetPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        TargetPlayer();
+        
     }
     public override void Action()
     {
@@ -22,5 +23,18 @@ public class EasyJim : BaseEnemyBehavior
     public override void TakeDamage()
     {
         
+    }
+    private void TargetPlayer()
+    {
+        // Vector3 target = new Vector3(0, 0, 0);
+        // transform.LookAt(new Vector3(target.x, 0, target.z));
+        Vector3 lookPos = new Vector3(0, 0, 0) - transform.position;
+        lookPos.y = 0f;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10);
+
+        // Vector3 lookDir = -transform.position;
+        // float angle = Mathf.Atan2(lookDir.z, lookDir.x) * Mathf.Rad2Deg;
+        // transform.rotation = Quaternion.Euler(0, angle, 0);
     }
 }
