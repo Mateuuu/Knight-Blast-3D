dfdcfa5452dbd8b5042587bfac291d3137a27656
Mateuuu <80788450+Mateuuu@users.noreply.github.com>
2022-02-09 21:57:01 -0700

    Making the blocking system better

--- Knight Blast 3D/Assets/Scripts/Player.cs
+++ Knight Blast 3D/Assets/Scripts/Player.cs
@@ -8,6 +8,14 @@ public class Player : MonoBehaviour
     // Hit Windows has a variable called "inHitbox" if this is true, that means that the arrows can be blocked by the shield
     [SerializeField] private HitWindow[] hitWindows;
     [SerializeField] private GameObject shield;
+
+
+    Vector3 topRight = new Vector3(1, 1, .5f);
+    Vector3 topLeft = new Vector3(-1, 1, .5f);
+    Vector3 bottomLeft = new Vector3(-1, 1, -.5f);
+    Vector3 bottomRight = new Vector3(1, 1, -.5f);
+
+
     private void Awake()
     {
         ObjectPool.NewPool("Arrow", arrowPrefab);
@@ -16,22 +24,38 @@ public class Player : MonoBehaviour
     // ! This is probably bad, and I should probably fix it, but for now, sucks to be me
     void Update()
     {
-        if(hitWindows[0].inHitbox && Input.GetKeyDown(KeyCode.Y))
+        shield.gameObject.layer = 10;
+        if(Input.GetKeyDown(KeyCode.Y))
         {
-            shield.transform.position = new Vector3(1, 1, .5f);
+            shield.transform.position = topRight;
+            if(hitWindows[0].inHitbox)
+            {
+                shield.gameObject.layer = 8;
+            }
         }
-        if(hitWindows[1].inHitbox && Input.GetKeyDown(KeyCode.T))
+        if(Input.GetKeyDown(KeyCode.T))
         {
-            shield.transform.position = new Vector3(-1, 1, .5f);
+            shield.transform.position = topLeft;
+            if(hitWindows[1].inHitbox)
+            {
+                shield.gameObject.layer = 8;
+            }
         }
-        if(hitWindows[2].inHitbox && Input.GetKeyDown(KeyCode.G))
+        if(Input.GetKeyDown(KeyCode.G))
         {
-            shield.transform.position = new Vector3(-1, 1, -.5f);
+            shield.transform.position = bottomLeft;
+            if(hitWindows[2].inHitbox)
+            {
+                shield.gameObject.layer = 8;
+            }
         }
-        if(hitWindows[3].inHitbox && Input.GetKeyDown(KeyCode.H))
+        if(Input.GetKeyDown(KeyCode.H))
         {
-            shield.transform.position = new Vector3(1, 1, -.5f);
+            shield.transform.position = bottomRight;
+            if(hitWindows[3].inHitbox)
+            {
+                shield.gameObject.layer = 8;
+            }
         }
-
     }
 }
